1052b4c40890214987336682fa279b6ff5d354c0
Pila <pat@medicuja.com>
2020-10-20 05:21:46 +0200

    Implement caching

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,18 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+  mongoURI: 'mongodb://pila:KEjHTKtNH0GzwhVJ@cluster0-shard-00-00.nwhpw.mongodb.net:27017,cluster0-shard-00-01.nwhpw.mongodb.net:27017,cluster0-shard-00-02.nwhpw.mongodb.net:27017/node_blog?ssl=true&replicaSet=atlas-1wouqh-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123', 
+  // KEjHTKtNH0GzwhVJ
 };
+
+
+// var MongoClient = require('mongodb').MongoClient;
+
+// var uri = "mongodb://pila:<password>@cluster0-shard-00-00.nwhpw.mongodb.net:27017,cluster0-shard-00-01.nwhpw.mongodb.net:27017,cluster0-shard-00-02.nwhpw.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-1wouqh-shard-0&authSource=admin&retryWrites=true&w=majority";
+// MongoClient.connect(uri, function(err, client) {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+

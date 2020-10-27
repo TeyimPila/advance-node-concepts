ded9707c8bb7ea3f29557bc1768592084da21d1b
Pila <pat@medicuja.com>
2020-10-27 06:32:43 +0100

    Teaser into testing with puppeteer

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,29 @@
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
+const puppeteer = require('puppeteer')
 
-    expect(sum).toEqual(3)
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage()
+    await page.goto('localhost:3000')
+})
+
+afterEach(async () => {
+    await browser.close();
+})
+
+
+test('The header has the correct text', async () => {
+    const logoText = await page.$eval('a.brand-logo', element => element.innerHTML);
+
+    expect(logoText).toEqual('Blogster');
+})
+
+test('Clicking login starts Oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = page.url();
+    // expect(url).toMatch('/accounts\.google\.com/')
 })
